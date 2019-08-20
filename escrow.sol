8ee826c3252baebcdd7d71254b703a9d63d3f110
oyincode <oyindadavid18@gmail.com>
2019-08-20 04:39:39 +0100

    refundBuyer Function

--- escrow.sol
+++ escrow.sol
@@ -2,16 +2,21 @@ pragma solidity >=0.4.0 <0.7.0;
 
 contract Escrow{
     
-    enum State {AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETED, REFUNDED}
+    enum State {AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETE, REFUNDED}
     State public currentState;
     
     
     address payable buyer;
     address payable seller;
-    address public arbiter;
+    address payable arbiter;
     
      modifier buyerOnly(){
-        require(msg.sender == buyer);
+        require(msg.sender == buyer || msg.sender == arbiter );
+        _;
+    }
+    
+    modifier sellerOnly(){
+        require(msg.sender == seller || msg.sender == arbiter);
         _;
     }
     
@@ -20,7 +25,7 @@ contract Escrow{
         _;
     }
     
-    constructor(address payable _buyer, address payable _seller, address _arbiter) public {
+    constructor(address payable _buyer, address payable _seller, address payable _arbiter) public {
         buyer = _buyer;
         seller = _seller;
         arbiter = _arbiter;
@@ -31,10 +36,14 @@ contract Escrow{
     }
     
     function confirmDelivery() public buyerOnly inState(State.AWAITING_DELIVERY){
-        currentState = State.COMPLETED;
+        currentState = State.COMPLETE;
         seller.transfer(address(this).balance);
     }
     
+    function refundBuyer() public sellerOnly inState(State.AWAITING_DELIVERY){
+        currentState = State.REFUNDED;
+        buyer.transfer(address(this).balance);
+    }
     
     
 }
