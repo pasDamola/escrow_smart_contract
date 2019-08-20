1df4c625181cf1cffe5760da2fb1f7f8a9fbd4e3
oyincode <oyindadavid18@gmail.com>
2019-08-20 04:19:12 +0100

    sendPayment function

--- escrow.sol
+++ escrow.sol
@@ -1,11 +1,13 @@
 pragma solidity >=0.4.0 <0.7.0;
 
 contract Escrow{
+    
     enum State {AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETED, REFUNDED}
     State public currentState;
     
-    address public buyer;
-    address public seller;
+    
+    address payable buyer;
+    address payable seller;
     address public arbiter;
     
      modifier buyerOnly(){
@@ -18,14 +20,21 @@ contract Escrow{
         _;
     }
     
-    constructor(address _buyer, address _seller, address _arbiter) public {
+    constructor(address payable _buyer, address payable _seller, address _arbiter) public {
         buyer = _buyer;
         seller = _seller;
         arbiter = _arbiter;
     }
     
-    function sendPayment() public payable buyerOnly inState(State.AWAITING_DELIVERY) {
-        
+    function sendPayment() public payable buyerOnly inState(State.AWAITING_PAYMENT) {
+     currentState = State.AWAITING_DELIVERY;   
     }
+    
+    function confirmDelivery() public buyerOnly inState(State.AWAITING_DELIVERY){
+        currentState = State.COMPLETED;
+        seller.transfer(address(this).balance);
+    }
+    
+    
     
 }
