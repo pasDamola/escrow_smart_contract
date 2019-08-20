4427d31e6b44ebb585f2077f2d487e5feb76eed8
oyincode <oyindadavid18@gmail.com>
2019-08-20 15:16:06 +0100

    comments

--- escrow.sol
+++ escrow.sol
@@ -1,8 +1,9 @@
 pragma solidity >=0.4.0 <0.7.0;
 
 contract Escrow{
-    
+    // different states of the escrow transaction
     enum State {AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETE, REFUNDED}
+
     State public currentState;
     
     
@@ -10,11 +11,12 @@ contract Escrow{
     address payable seller;
     address payable arbiter;
     
+    //defining buyer's rights
      modifier buyerOnly(){
         require(msg.sender == buyer || msg.sender == arbiter );
         _;
     }
-    
+    //defining seller's rights
     modifier sellerOnly(){
         require(msg.sender == seller || msg.sender == arbiter);
         _;
@@ -31,15 +33,17 @@ contract Escrow{
         arbiter = _arbiter;
     }
     
+    //buyer is to pay agreed money to the arbiter before delivery of goods
     function sendPayment() public payable buyerOnly inState(State.AWAITING_PAYMENT) {
      currentState = State.AWAITING_DELIVERY;   
     }
     
+    //buyer is to confirm delivery of goods/services from seller before money is sent to the seller
     function confirmDelivery() public buyerOnly inState(State.AWAITING_DELIVERY){
         currentState = State.COMPLETE;
         seller.transfer(address(this).balance);
     }
-    
+    // if the goods/services offered by the seller are sub-standard, arbiter refunds money to buyer
     function refundBuyer() public sellerOnly inState(State.AWAITING_DELIVERY){
         currentState = State.REFUNDED;
         buyer.transfer(address(this).balance);
